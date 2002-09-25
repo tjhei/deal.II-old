77ca39dadd70cbfa5e55b3fec6da4c96da056653
wolf <wolf@0785d39b-7218-0410-832d-ea1e28bc413d>
2002-09-25 16:08:09 +0000

    Add header to file.
    git-svn-id: https://svn.dealii.org/trunk@6510 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/contrib/hsl/source/detached_ma27.cc
+++ deal.II/contrib/hsl/source/detached_ma27.cc
@@ -1,3 +1,16 @@
+//----------------------------  detached_ma27.cc  ---------------------------
+//    $Id$
+//    Version: $Name$
+//
+//    Copyright (C) 2002 by the deal.II authors
+//
+//    This file is subject to QPL and may not be  distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//----------------------------  detached_ma27.cc  ---------------------------
+
 #include <cstdio>
 #include <unistd.h>
 #include <hsl/hsl.h>

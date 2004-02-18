de52b03836463d6808ff872b913ff0506538903b
wolf <wolf@0785d39b-7218-0410-832d-ea1e28bc413d>
2004-02-18 15:33:50 +0000

    Don't check for newer gccs at all...
    git-svn-id: https://svn.dealii.org/trunk@8479 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/contrib/boost/include/boost/config/compiler/gcc.hpp
+++ deal.II/contrib/boost/include/boost/config/compiler/gcc.hpp
@@ -72,13 +72,4 @@
 #if (__GNUC__ == 2) && (__GNUC_MINOR__ < 90)
 #  error "Compiler not configured - please reconfigure"
 #endif
-//
-// last known and checked version is 3.4:
-#if (__GNUC__ > 3) || ((__GNUC__ == 3) && (__GNUC_MINOR__ > 4))
-#  if defined(BOOST_ASSERT_CONFIG)
-#     error "Unknown compiler version - please run the configure tests and report the results"
-#  else
-#     warning "Unknown compiler version - please run the configure tests and report the results"
-#  endif
-#endif
 

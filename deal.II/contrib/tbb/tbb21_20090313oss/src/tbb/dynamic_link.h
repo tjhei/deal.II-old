0a428910dd3dcb76bd8a8e501e2a09d1d754a0e4
bangerth <bangerth@0785d39b-7218-0410-832d-ea1e28bc413d>
2009-05-15 17:59:46 +0000

    Merge branch_threading_building_blocks.
    git-svn-id: https://svn.dealii.org/trunk@18849 0785d39b-7218-0410-832d-ea1e28bc413d

---
+++ deal.II/contrib/tbb/tbb21_20090313oss/src/tbb/dynamic_link.h
@@ -0,0 +1,89 @@
+/*
+    Copyright 2005-2009 Intel Corporation.  All Rights Reserved.
+
+    This file is part of Threading Building Blocks.
+
+    Threading Building Blocks is free software; you can redistribute it
+    and/or modify it under the terms of the GNU General Public License
+    version 2 as published by the Free Software Foundation.
+
+    Threading Building Blocks is distributed in the hope that it will be
+    useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+    of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Threading Building Blocks; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+    As a special exception, you may use this file as part of a free software
+    library without restriction.  Specifically, if other files instantiate
+    templates or use macros or inline functions from this file, or you compile
+    this file and link it with other files to produce an executable, this
+    file does not by itself cause the resulting executable to be covered by
+    the GNU General Public License.  This exception does not however
+    invalidate any other reasons why the executable file might be covered by
+    the GNU General Public License.
+*/
+
+#ifndef __TBB_dynamic_link
+#define __TBB_dynamic_link
+
+// Support for dynamically linking to a shared library.
+// By default, the symbols defined here go in namespace tbb::internal.
+// The symbols can be put in another namespace by defining the preprocessor
+// symbols OPEN_INTERNAL_NAMESPACE and CLOSE_INTERNAL_NAMESPACE to open and
+// close the other namespace.  See default definition below for an example.
+
+#ifndef OPEN_INTERNAL_NAMESPACE
+#define OPEN_INTERNAL_NAMESPACE namespace tbb { namespace internal {
+#define CLOSE_INTERNAL_NAMESPACE }}
+#endif /* OPEN_INTERNAL_NAMESPACE */
+
+#include <stddef.h>
+
+OPEN_INTERNAL_NAMESPACE
+
+//! Type definition for a pointer to a void somefunc(void)
+typedef void (*pointer_to_handler)();
+
+// Double cast through the void* from func_ptr in DLD macro is necessary to 
+// prevent warnings from some compilers (g++ 4.1)
+#if __TBB_WEAK_SYMBOLS
+
+#define DLD(s,h) {(pointer_to_handler)&s, (pointer_to_handler*)(void*)(&h)}
+//! Association between a handler name and location of pointer to it.
+struct dynamic_link_descriptor {
+    //! pointer to the handler
+    pointer_to_handler ptr;
+    //! Pointer to the handler
+    pointer_to_handler* handler;
+};
+
+#else /* !__TBB_WEAK_SYMBOLS */
+
+#define DLD(s,h) {#s, (pointer_to_handler*)(void*)(&h)}
+//! Association between a handler name and location of pointer to it.
+struct dynamic_link_descriptor {
+    //! Name of the handler
+    const char* name;
+    //! Pointer to the handler
+    pointer_to_handler* handler;
+};
+
+#endif /* !__TBB_WEAK_SYMBOLS */
+
+//! Fill in dynamically linked handlers.
+/** 'n' is the length of the array descriptors[].
+    'required' is the number of the initial entries in the array descriptors[] 
+    that have to be found in order for the call to succeed. If the library and 
+    all the required handlers are found, then the corresponding handler pointers 
+    are set. Otherwise the original array of descriptors is left untouched. **/
+bool dynamic_link( const char* libraryname, 
+                   const dynamic_link_descriptor descriptors[], 
+                   size_t n, 
+                   size_t required = ~(size_t)0 );
+
+CLOSE_INTERNAL_NAMESPACE
+
+#endif /* __TBB_dynamic_link */

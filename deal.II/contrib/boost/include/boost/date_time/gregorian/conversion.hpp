89177fd94b9d15b264e833a591259ff12dbd4036
bangerth <bangerth@0785d39b-7218-0410-832d-ea1e28bc413d>
2010-07-01 03:35:26 +0000

    Re-apply patch 20228 to new version of boost.
    git-svn-id: https://svn.dealii.org/trunk@21427 0785d39b-7218-0410-832d-ea1e28bc413d

--- deal.II/contrib/boost/include/boost/date_time/gregorian/conversion.hpp
+++ deal.II/contrib/boost/include/boost/date_time/gregorian/conversion.hpp
@@ -41,7 +41,7 @@ namespace gregorian {
         boost::throw_exception(std::out_of_range(s));
     }
 
-    std::tm datetm = {}; // zero initialization is needed for extension members, like tm_zone
+    std::tm datetm = std::tm(); // zero initialization is needed for extension members, like tm_zone
     boost::gregorian::date::ymd_type ymd = d.year_month_day();
     datetm.tm_year = ymd.year - 1900;
     datetm.tm_mon = ymd.month - 1;

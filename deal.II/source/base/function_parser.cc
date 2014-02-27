tjhei/deal.II-old

commit 2ea52fe80ebd4a9982724a23b2bfc63add7be8f5

Improve speed a little more by using add_entries in distributed version of Cuthill_McKee renumbering. Add some assertions.
git-svn-id: https://svn.dealii.org/trunk@32549 0785d39b-7218-0410-832d-ea1e28bc413d

deal.II/source/lac/sparsity_pattern.cc
@@ -789,7 +789,8 @@ SparsityPattern::add_entries (const size_type row,
         {
           ForwardIterator it = begin;
           bool has_larger_entries = false;
-          std::size_t k=rowstart[row];
+          // skip diagonal
+          std::size_t k=rowstart[row]+store_diagonal_first_in_row;
           for ( ; k<rowstart[row+1]; k++)
             if (colnums[k] == invalid_entry)
               break;

deal.II/include/deal.II/lac/full_matrix.h
@@ -1195,7 +1195,7 @@ FullMatrix<number>::operator = (const number d)
   (void)d; // removes -Wunused-parameter warning in optimized mode
 
   if (this->n_elements() != 0)
-    std::memset (&this->values[0], 0, this->n_elements()*sizeof(number));
+    this->reset_values();
 
   return *this;
 }

deal.II/source/dofs/dof_renumbering.cc
@@ -420,13 +420,17 @@ namespace DoFRenumbering
               row_lengths[i] = csp.row_length(locally_owned.nth_index_in_set(i));
             sparsity.reinit(locally_owned.n_elements(), locally_owned.n_elements(),
                             row_lengths);
+            std::vector<types::global_dof_index> row_entries;
             for (unsigned int i=0; i<locally_owned.n_elements(); ++i)
               {
                 const types::global_dof_index row = locally_owned.nth_index_in_set(i);
+                row_entries.resize(0);
                 for (CompressedSimpleSparsityPattern::row_iterator it =
                        csp.row_begin(row); it != csp.row_end(row); ++it)
                   if (*it != row && locally_owned.is_element(*it))
-                    sparsity.add(i, locally_owned.index_within_set(*it));
+                    row_entries.push_back(locally_owned.index_within_set(*it));
+                sparsity.add_entries(i, row_entries.begin(), row_entries.end(),
+                                     true);
               }
             sparsity.compress();
           }

deal.II/source/lac/lapack_full_matrix.cc
@@ -93,7 +93,7 @@ LAPACKFullMatrix<number>::operator = (const double d)
   Assert (d==0, ExcScalarAssignmentOnlyForZeroValue());
 
   if (this->n_elements() != 0)
-    std::fill (this->values.begin(), this->values.end(), number());
+    this->reset_values();
 
   state = LAPACKSupport::matrix;
   return *this;
@@ -551,6 +551,7 @@ LAPACKFullMatrix<number>::apply_lu_factorization(Vector<number> &v,
   Assert(state == lu, ExcState(state));
   Assert(this->n_rows() == this->n_cols(),
          LACExceptions::ExcNotQuadratic());
+  AssertDimension(this->n_rows(), v.size());
 
   const char *trans = transposed ? &T : &N;
   const int nn = this->n_cols();
@@ -573,6 +574,7 @@ LAPACKFullMatrix<number>::apply_lu_factorization(LAPACKFullMatrix<number> &B,
   Assert(state == lu, ExcState(state));
   Assert(B.state == matrix, ExcState(state));
   Assert(this->n_rows() == this->n_cols(), LACExceptions::ExcNotQuadratic());
+  AssertDimension(this->n_rows(), B.n_rows());
 
   const char *trans = transposed ? &T : &N;
   const int nn = this->n_cols();

commit 3e774e36ab5747a57388a8dac32e49867b6a407f

Fix compile error + fix severe bug introduced in r32549
git-svn-id: https://svn.dealii.org/trunk@32564 0785d39b-7218-0410-832d-ea1e28bc413d

deal.II/source/base/function_parser.cc
@@ -172,11 +172,11 @@ void FunctionParser<dim>:: init_muparser() const
 	}
       catch (mu::ParserError &e)
 	{
-	  cerr << "Message:  " << e.GetMsg() << "\n";
-	  cerr << "Formula:  " << e.GetExpr() << "\n";
-	  cerr << "Token:    " << e.GetToken() << "\n";
-	  cerr << "Position: " << e.GetPos() << "\n";
-	  cerr << "Errc:     " << e.GetCode() << std::endl;	  
+          std::cerr << "Message:  " << e.GetMsg() << "\n";
+	  std::cerr << "Formula:  " << e.GetExpr() << "\n";
+	  std::cerr << "Token:    " << e.GetToken() << "\n";
+	  std::cerr << "Position: " << e.GetPos() << "\n";
+	  std::cerr << "Errc:     " << e.GetCode() << std::endl;	  
 	  AssertThrow(false, ExcParseError(e.GetCode(), e.GetMsg().c_str()));
 	}      
     }
@@ -306,11 +306,11 @@ double FunctionParser<dim>::value (const Point<dim>  &p,
     }
   catch (mu::ParserError &e)
     {
-      cerr << "Message:  " << e.GetMsg() << "\n";
-      cerr << "Formula:  " << e.GetExpr() << "\n";
-      cerr << "Token:    " << e.GetToken() << "\n";
-      cerr << "Position: " << e.GetPos() << "\n";
-      cerr << "Errc:     " << e.GetCode() << std::endl;	  
+      std::cerr << "Message:  " << e.GetMsg() << "\n";
+      std::cerr << "Formula:  " << e.GetExpr() << "\n";
+      std::cerr << "Token:    " << e.GetToken() << "\n";
+      std::cerr << "Position: " << e.GetPos() << "\n";
+      std::cerr << "Errc:     " << e.GetCode() << std::endl;	  
       AssertThrow(false, ExcParseError(e.GetCode(), e.GetMsg().c_str()));
       return 0.0;
     }

deal.II/source/lac/sparsity_pattern.cc
@@ -800,11 +800,13 @@ SparsityPattern::add_entries (const size_type row,
                 break;
               }
           if (has_larger_entries == false)
-            for ( ; it != end; ++it, ++k)
+            for ( ; it != end; ++it)
               {
+                if (*it == row)
+                  continue;
                 Assert (k <= rowstart[row+1],
                         ExcNotEnoughSpace(row, rowstart[row+1]-rowstart[row]));
-                colnums[k] = *it;
+                colnums[k++] = *it;
               }
           else
             // cannot just append the new range at the end, forward to the